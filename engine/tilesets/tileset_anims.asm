24a6713c2ad8dc5b565e2a7f48d35ffbe11453c0
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-05-03 22:43:52 +0700

    yeet the tileset animations

--- engine/tilesets/tileset_anims.asm
+++ engine/tilesets/tileset_anims.asm
@@ -61,18 +61,18 @@ TilesetForestAnim::
 	dw NULL,  DoneTileAnimation
 
 TilesetJohtoAnim:
-	dw vTiles2 tile $14, AnimateWaterTile
-	dw NULL,  WaitTileAnimation
-	dw NULL,  WaitTileAnimation
-	dw NULL,  AnimateWaterPalette
-	dw NULL,  WaitTileAnimation
-	dw NULL,  AnimateFlowerTile
-	dw WhirlpoolFrames1, AnimateWhirlpoolTile
-	dw WhirlpoolFrames2, AnimateWhirlpoolTile
-	dw WhirlpoolFrames3, AnimateWhirlpoolTile
-	dw WhirlpoolFrames4, AnimateWhirlpoolTile
-	dw NULL,  WaitTileAnimation
-	dw NULL,  StandingTileFrame8
+;	dw vTiles2 tile $14, AnimateWaterTile
+;	dw NULL,  WaitTileAnimation
+;	dw NULL,  WaitTileAnimation
+;	dw NULL,  AnimateWaterPalette
+;	dw NULL,  WaitTileAnimation
+;	dw NULL,  AnimateFlowerTile
+	;dw WhirlpoolFrames1, AnimateWhirlpoolTile
+	;dw WhirlpoolFrames2, AnimateWhirlpoolTile
+	;dw WhirlpoolFrames3, AnimateWhirlpoolTile
+	;dw WhirlpoolFrames4, AnimateWhirlpoolTile
+;	dw NULL,  WaitTileAnimation
+;	dw NULL,  StandingTileFrame8
 	dw NULL,  DoneTileAnimation
 
 UnusedTilesetAnim1: ; unreferenced
